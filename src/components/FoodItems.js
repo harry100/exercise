59ca2843e685107b5dbb2bb946c223b870e281f4
Amanda Maharjan <maharjanamanda@gmail.com>
2019-11-28 22:00:46 +0545

    setup placeholder

--- src/components/FoodItems.js
+++ src/components/FoodItems.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 //constants
@@ -7,20 +7,70 @@ import constants from '../utils/constants';
 //includes
 import FoodCard from './includes/FoodCard';
 
-function FoodItem (){
+const items = [
+    {
+        image: '/images/giphy.gif',
+        price: '',
+        name: ''
+    },
+    {
+        image: '/images/giphy.gif',
+        price: '',
+        name: ''
+    }
+]
+
+const FoodItem = () => {
+    const [foodItems, setFoodItems] = useState(items);
+
     useEffect(() => {
         axios.get(constants.getAllFoodItems)
             .then(result => {
-                console.log(result)
+                setFoodItems(result.data)
             })
             .catch(err => console.log(err))
     }, [])
+
     return(
         <div className="container">
             <h1 className="food-item-heading"> Checkout today's awesome items </h1>
-            <div className="row">
-                <FoodCard />
-            </div>
+
+            <section>
+                <div className="row">
+                    {
+                        foodItems.map((item, i) => {
+                            return <FoodCard key={i} item={item} />
+                        })
+                    }
+                </div>
+            </section>
+
+            <section>
+                {foodItems.length>2? (
+                    <div className="navigation">
+                        <nav aria-label="Page navigation example" className="pagination-nav">
+                            <ul className="pagination">
+                                <li className="page-item">
+                                <a className="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                </li>
+                                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                <li className="page-item"><a className="page-link" href="#">2</a></li>
+                                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                <li className="page-item">
+                                <a className="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span className="sr-only">Next</span>
+                                </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                ): ('')}
+            </section>
+
         </div>
     )
 }
--- src/components/includes/FoodCard.js
+++ src/components/includes/FoodCard.js
@@ -1,26 +1,48 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function FoodCard(){
-
+const FoodCard = (props) => {
     return(
-        <div className="col-sm-12 col-md-4 food-card-wrapper">
-            <div className="row">
+        <div className="col-sm-12 col-md-6 food-card-wrapper">
+            {props.item.id? (
+                <div className="food-card-cont">
+                    <div className="col-sm-12 col-md-4 food-card-image-wrapper">
+                        <div className="food-item-card">
+                            <div className="food-item-image">
+                                <img src={props.item.image} alt={props.item.name} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-8">
+                        <div className="food-item-desc">
+                            <h3> {props.item.name} </h3>
+                            <p> Price: Nrs. <strong>{props.item.price}</strong> </p>
+                        </div>
+                    </div>
+                </div>
+            ): (
+                <div className="food-card-cont skeleton">
                 <div className="col-sm-12 col-md-4 food-card-image-wrapper">
                     <div className="food-item-card">
                         <div className="food-item-image">
-                            <img src="/images/giphy.gif" alt="food"/>
+                            <img src={props.item.image} alt={props.item.name} />
                         </div>
                     </div>
                 </div>
                 <div className="col-sm-12 col-md-8">
                     <div className="food-item-desc">
-                        <h3> Chicken Shawarma </h3>
-                        <p> Price: Rs. 190 </p>
+                        <p />
+                        <p />
                     </div>
                 </div>
             </div>
+            )}
         </div>
     )
+}
+
+FoodCard.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default FoodCard;
