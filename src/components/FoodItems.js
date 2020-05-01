04279eb315a4cf66bd88b71d17854c3942714c04
Amanda Maharjan <maharjanamanda@gmail.com>
2020-05-01 21:45:22 +0545

    auto deploy testing

--- src/components/FoodItems.js
+++ src/components/FoodItems.js
@@ -66,7 +66,7 @@ const FoodItem = () => {
                         paginate={paginate}
                         activePage={currentPage}
                     />
-                ): ('Data blocked Need to create a backend')}
+                ): ('Data blocked Need to create a backend api')}
             </section>
 
         </div>
